910f9c7a7ada4c3c45f0cd8f03947012271f1b93
Fer_Viudez <fernando.viudez@indelve.com>
2023-05-26 17:40:15 -0300

    Ejercicios 4 & 5

--- main.hs
+++ main.hs
@@ -11,8 +11,8 @@ data Zombie = Zombie
     poderMordida :: Int
   }
   deriving (Show, Eq)
-vidaZombie = length . nombre
 
+vidaZombie = length . nombre
 
 data LineaDeDefensa = LineaDeDefensa
   { plantas :: [Planta],
@@ -22,23 +22,32 @@ data LineaDeDefensa = LineaDeDefensa
 
 -- Plantas
 peashooter = Planta 5 0 2
+
 repeater = Planta 5 0 4
+
 sunflower = Planta 7 1 0
+
 nut = Planta 30 0 0
+
 cactus = Planta 9 0 0
+
 rose = Planta 2 45 7
 
 -- Zombies
-zombieBase = Zombie "Base" [] 1 
-zombieBaloon = Zombie "Balloon Zombie" ["Globo"] 1 
-zombieNewspaper :: Zombie
-zombieNewspaper = Zombie "Newspaper Zombie" ["Diario"] 2 
+zombieBase = Zombie "Base" [] 1
+
+zombieBaloon = Zombie "Balloon Zombie" ["Globo"] 1
+
+zombieNewspaper = Zombie "Newspaper Zombie" ["Diario"] 2
+
 gargantuar = Zombie "Gargantuar Hulk Smash Puny God" ["Poste de luz", "zombie enano"] 30
 
---Lineas 
-linea1 = LineaDeDefensa {plantas = [], zombies = [] }
+-- Lineas
+linea1 = LineaDeDefensa {plantas = [], zombies = []}
+
 linea2 = LineaDeDefensa {plantas = [], zombies = []}
-linea3 =LineaDeDefensa {plantas = [], zombies = [] }
+
+linea3 = LineaDeDefensa {plantas = [], zombies = []}
 
 -- funciones utils (genericas para varias soluciones)
 listaEstaVacia [] = True
@@ -48,7 +57,7 @@ obtenerDanioTotal [] _ = 0
 obtenerDanioTotal (elemento : cola) obtenerCampoPoder = (obtenerCampoPoder elemento) + obtenerDanioTotal cola obtenerCampoPoder
 
 modificarPosicionDeLista _ _ [] = []
-modificarPosicionDeLista index nuevoValor (x:xs)
+modificarPosicionDeLista index nuevoValor (x : xs)
   | index == 0 = nuevoValor : xs
   | otherwise = x : modificarPosicionDeLista (index - 1) nuevoValor xs
 
@@ -58,10 +67,11 @@ especialidad (Planta vida cantSoles poder)
   | poder > vida = "Atacante"
   | otherwise = "Defensiva"
 
-
 -- 2) b.
 tieneVidaMayorQueDiez = (> 10) . vidaZombie
-tieneAccesorios =  (>= 1) . length . accesorios
+
+tieneAccesorios = (>= 1) . length . accesorios
+
 esPeligroso zombie = tieneAccesorios zombie || tieneVidaMayorQueDiez zombie
 
 -- 3) a.
@@ -78,90 +88,91 @@ todosLosZombiesSonPeligrosos linea = (not . listaEstaVacia . zombies) linea && (
 
 estaEnPeligro linea = poderDePlantasEsMenorQuePoderDeZombies linea || todosLosZombiesSonPeligrosos linea
 
-
 -- 3) c.
 plantasSonProveedoras [] = True
 plantasSonProveedoras (planta : cola) = especialidad planta == "Proveedora" && plantasSonProveedoras cola
 
 lineaNecesitaDefensa linea = (listaEstaVacia) listaDePlantas || plantasSonProveedoras listaDePlantas
-  where 
+  where
     listaDePlantas = plantas linea
-
 
 -- 4)
 plantasConsecutivasTieneDistintaEspecialidad [] _ = True
 plantasConsecutivasTieneDistintaEspecialidad (planta : cola) [] = plantasConsecutivasTieneDistintaEspecialidad cola (especialidad planta)
 plantasConsecutivasTieneDistintaEspecialidad (planta : cola) especialidadAnterior = especialidadAnterior /= (especialidad planta) && plantasConsecutivasTieneDistintaEspecialidad cola (especialidad planta)
 
--- sin usar length ni alguna funcion simil propia de haskell, 
+-- sin usar length ni alguna funcion simil propia de haskell,
 -- contar la cantidad de elementos de una lista
 cantidadElementos [] = 0
-cantidadElementos (elemento: cola) = 1 + cantidadElementos cola
+cantidadElementos (elemento : cola) = 1 + cantidadElementos cola
 
 esMixta linea = (cantidadElementos . plantas) linea >= 2 && (plantasConsecutivasTieneDistintaEspecialidad . plantas) linea []
 
-
 -- 5)
 restarVidaAObjeto objeto cantidadARestar registroVida funcionRestarVidaObjeto atacante = funcionRestarVidaObjeto objeto cantidadFinalARestar atacante
-  where 
-   vidaNuevaDelObjeto = registroVida objeto - cantidadARestar
-   cantidadFinalARestar = if vidaNuevaDelObjeto <= 0 then registroVida objeto else cantidadARestar
+  where
+    vidaNuevaDelObjeto = registroVida objeto - cantidadARestar
+    cantidadFinalARestar = if vidaNuevaDelObjeto <= 0 then registroVida objeto else cantidadARestar
 
 -- a.
 -- filtrar accesorios recibe el elemento y devuelve la condicion de filtrado
 
+condicionFiltradoPorPlanta (Planta 9 0 0) = (/= "Globo")
+condicionFiltradoPorPlanta Planta {} = (/= "")
+
 esCactus (Planta 9 0 0) = True
-esCactus (Planta _ _ _) = False
--- esCactus planta = vida planta == 9 && cantSoles planta == 0 && poder planta == 0
-restarVidaAZombie zombie cantidadARestar plantaAtacante = zombie {
-  nombre = drop cantidadARestar (nombre zombie),
-  accesorios=[
-    accesorio | accesorio <- accesorios zombie, 
-    (esCactus plantaAtacante && accesorio /= "Globo") || ((not.esCactus) plantaAtacante) 
-  ]
-}
+esCactus Planta {} = False
+
+restarVidaAZombie zombie cantidadARestar plantaAtacante =
+  zombie
+    { nombre = drop cantidadARestar (nombre zombie),
+      accesorios =
+        [ accesorio | accesorio <- accesorios zombie, condicionFiltradoPorPlanta plantaAtacante accesorio
+        ]
+    }
 
 plantaMataZombie zombie planta = restarVidaAObjeto zombie (poder planta) vidaZombie restarVidaAZombie planta
 
 -- b.
-restarVidaAPlanta planta cantidadARestar _ = planta {
-  vida = vida planta - cantidadARestar
-}
-zombieMataPlanta planta zombie = restarVidaAObjeto planta (poderMordida zombie) vida restarVidaAPlanta zombie
+restarVidaAPlanta planta cantidadARestar _ =
+  planta
+    { vida = vida planta - cantidadARestar
+    }
 
+zombieMataPlanta planta zombie = restarVidaAObjeto planta (poderMordida zombie) vida restarVidaAPlanta zombie
 
 -- ### parte 2 ###
 
 -- 1. I) evaluamos usando la estrategia call-by-name y sharing (como evalua haskell)
 -- estaEnPeligro linea = poderDePlantasEsMenorQuePoderDeZombies linea || todosLosZombiesSonPeligrosos linea
--- 
+--
 -- estaEnPeligro LineaDeDefensa [] [zombieBase, ...]
--- 
--- aplicamos estaEnPeligro 
+--
+-- aplicamos estaEnPeligro
 -- poderDePlantasEsMenorQuePoderDeZombies linea || todosLosZombiesSonPeligrosos linea
--- 
+--
 -- como || es estricta, aplicamos poderDePlantasEsMenorQuePoderDeZombies
 -- poderDePlantasEsMenorQuePoderDeZombies linea = obtenerDanioTotal (plantas linea) poder < obtenerDanioTotal (zombies linea) poderMordida
--- 
+--
 -- luego como < también es estricta, aplicamos obtenerDanioTotal
 -- obtenerDanioTotal [] poder
--- 
+--
 -- 0
--- 
+--
 -- queda asi:
 -- 0 < obtenerDanioTotal [zombieBase, ...] poderMordida
--- 
+--
 -- aplicamos lo de la derecha ("obtenerDanioTotal")
 -- obtenerDanioTotal (elemento : cola) obtenerCampoPoder = (obtenerCampoPoder elemento) + obtenerDanioTotal cola obtenerCampoPoder
--- 
+--
 -- 1 + obtenerDanioTotal [zombieBase, ...] poderMordida
--- 
+--
 -- 1 +  (1 + obtenerDanioTotal [zombieBase, ...] poderMordida)
 
 -- 1 +  (1 +  (1 + obtenerDanioTotal [zombieBase, ...] poderMordida))
--- 
+--
 -- ...
--- 
+--
 -- como podemos notar, no termina nunca.
 
 -- II) a) evaluamos de igual manera que antes
@@ -197,7 +208,7 @@ zombieMataPlanta planta zombie = restarVidaAObjeto planta (poderMordida zombie) 
 --
 --
 --
--- II) b) 
+-- II) b)
 -- lineaNecesitaDefensa linea = (not. listaEstaVacia . plantas) linea || (plantasSonProveedoras . plantas) linea
 --
 -- lineaNecesitaDefensa (Linea [sunflower, ...] [])
@@ -220,15 +231,15 @@ zombieMataPlanta planta zombie = restarVidaAObjeto planta (poderMordida zombie) 
 -- especialidad planta == "Proveedora"
 --
 -- "Proveedora" == "Proveedora"
--- 
+--
 -- True
--- 
+--
 -- True && plantasSonProveedoras [sunflower, ...]
--- 
+--
 -- luego evaluamos la parte derecha
--- 
+--
 -- plantasSonProveedoras [sunflower, ...]
--- 
+--
 -- especialidad planta == "Proveedora"
 --
 -- "Proveedora" == "Proveedora"
@@ -236,22 +247,44 @@ zombieMataPlanta planta zombie = restarVidaAObjeto planta (poderMordida zombie) 
 -- True && (True && plantasSonProveedoras [sunflower, ...])
 --
 -- y asi sucesivamente, por lo que el procesamiento no termina.
+
 septimoRegimiento :: [(Zombie, Int)]
 septimoRegimiento = [(zombieNewspaper, 2), (zombieBaloon, 1), (zombieBaloon, 3)]
+
 region :: [(Zombie, Int)]
 region = [(gargantuar, 1), (gargantuar, 1), (gargantuar, 2), (gargantuar, 2), (gargantuar, 3), (gargantuar, 3)]
 
 jardin1 = [linea1, linea2, linea3]
 
+-- III)
 agregarHorda :: [LineaDeDefensa] -> [(Zombie, Int)] -> [LineaDeDefensa]
-agregarHorda jardin horda = foldl modificarJardin jardin horda
+agregarHorda = foldl agregarZombieAJardin
 
-modificarJardin :: [LineaDeDefensa] -> (Zombie, Int) -> [LineaDeDefensa]
-modificarJardin jardin (zombie, nroLinea) = 
+
+-- agregarZombieAJardin [linea1, linea2, linea3] (zombie, 1) = linea1 {zombies=agregarElementoALinea [zombie] linea1 zombies}:[linea2,linea3]
+
+-- agregarZombieAJardin [linea1, linea2, linea3] (zombie, 2) = [linea1] ++ [linea2 {zombies=agregarElementoALinea [zombie] linea2 zombies}] ++ [linea3]
+
+-- agregarZombieAJardin [linea1, linea2, linea3] (zombie, 3) = [linea1,linea2] ++ [linea3 {zombies=agregarElementoALinea [zombie] linea3 zombies}]
+
+agregarZombieAJardin :: [LineaDeDefensa] -> (Zombie, Int) -> [LineaDeDefensa]
+agregarZombieAJardin jardin (zombie, nroLinea) =
   modificarPosicionDeLista posicionDeLinea nuevaLinea jardin
-  where 
+  where
     posicionDeLinea = nroLinea - 1
-    linea = jardin !! posicionDeLinea
-    nuevosZombies = agregarElementoALinea [zombie] linea zombies
-    nuevaLinea = linea { zombies=nuevosZombies }
+    nuevaLinea = agregarZombieALinea zombie (jardin !! posicionDeLinea)
 
+agregarZombieALinea zombie linea = linea { zombies = agregarElementoALinea [zombie] linea zombies }
+
+-- IV)
+
+rondaDeAtaque :: Planta -> Zombie -> Int -> (Planta, Zombie)
+rondaDeAtaque planta zombie cantidadDeMordidas = (plantaLuegoDelAtaque, zombieLuegoDelAtaque)
+  where
+    zombieLuegoDelAtaque = plantaMataZombie zombie planta
+    nuevoZombie = zombieLuegoDelAtaque {poderMordida = poderMordida zombieLuegoDelAtaque * cantidadDeMordidas}
+    plantaLuegoDelAtaque = zombieMataPlanta planta nuevoZombie
+
+-- V)
+plantaMuere planta zombie cantMordidas = ((==0).vida.fst)(rondaDeAtaque planta zombie cantMordidas)
+zombieMuere planta zombie cantMordidas = ((==0).vidaZombie.snd)(rondaDeAtaque planta zombie cantMordidas)
